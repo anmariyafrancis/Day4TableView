9f3fbc402f82497afdced4e8a999ab95801cd996
anmariyafrancis <anmariya1915@gmail.com>
2020-03-04 21:10:57 -0500

    country and flag

---
+++ Day4TableView/Country.swift
@@ -0,0 +1,9 @@
+//
+//  Country.swift
+//  Day4TableView
+//
+//  Created by MacStudent on 2020-03-04.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
--- Day4TableView/ViewController.swift
+++ Day4TableView/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var tblCountry: UITableView!
+    
+    lazy var countryNames:[country]=[]
+        ["Afganistan","Angola","Aurba","Canada","Indai","Bahamas","Ghana","France","Nepal","Kenya"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func country()
+    {
+        
+    }
 }
+    extension ViewController:UITableViewDataSource,UITableViewDelegate
+    {
+        
+        func numberOfSections(in tableView:UITableView)->Int
+        {
+            return 1
+        }
+        func tableView(_ _tableView:UITableView,numberOfRowsInSection section:Int)->Int
+        {
+            return countryNames.count
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        {
+            let cell=tableView.dequeueReusableCell(withIdentifier: "CountryCell")
+            let country=countryNames[indexPath.row]
+            cell?.textLabel?.text=country.name
+            cell?.detailTextLabel?.text=country.capital
+            cell?.imageView?.image=country.flag
+            
+            return cell
+        }
+        
+    }
+    
+    
+
+
 
